ccf3e334218ece1804ef6474f2af6b2d1a4cc413
Jonathan Clarke <jc01663@surrey.ac.uk>
2020-03-22 19:36:50 +0000

    Added half-working devise with student and tutor logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :tutors, path: 'tutors', controllers: { sessions: "tutors/sessions"}
+  devise_for :students, path: 'students', controllers: { sessions: "students/sessions"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Home page is the root
   root 'home#home'
 
 end
