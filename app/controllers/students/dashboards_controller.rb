5a477c5c17dc20fc68123a5296664bd1ee07385d
xc00322 <xc00322@surrey.ac.uk>
2020-04-06 15:15:38 +0100

    Let the student choose their language

--- app/controllers/students/dashboards_controller.rb
+++ app/controllers/students/dashboards_controller.rb
@@ -1,4 +1,14 @@
 class Students::DashboardsController < ApplicationController
   def home
+    if current_student
+      @language = current_student.language
+    end
   end
+
+  def pickLanguage
+    lang = params[:lang]
+    current_student.language = lang
+    current_student.save
+  redirect_to authenticated_student_root_url
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
 
   authenticated :tutor do
     root 'tutors/dashboards#home', as: :authenticated_tutor_root
+    get 'dashboard', to: 'tutors/dashboards#home'
   end
 
   authenticated :student do
     root 'students/dashboards#home', as: :authenticated_student_root
+    post 'pickLanguage', to: 'students/dashboards#pickLanguage'
+    get 'dashboard', to: 'students/dashboards#home'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
