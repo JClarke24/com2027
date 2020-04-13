69891df68993e2b4667995d684cc983f68e8468a
xc00322 <xc00322@surrey.ac.uk>
2020-04-13 12:04:47 +0100

    Tutors can accept or decline student requests

--- app/controllers/students/dashboards_controller.rb
+++ app/controllers/students/dashboards_controller.rb
@@ -2,7 +2,13 @@ class Students::DashboardsController < ApplicationController
   def home
     if current_student
       @language = current_student.language
+      @tutor = Tutor.find(current_student.tutor_id)
+      @tutor_confirmed = current_student.tutor_confirmed
     end
+  rescue ActiveRecord::RecordNotFound
+    current_student.tutor_id = nil
+    current_student.tutor_confirmed = false
+    current_student.save
   end
 
   def pickLanguage
@@ -14,6 +20,7 @@ end
 def requestTutor
   tutor_id = params[:tutor_id]
   current_student.tutor_id = tutor_id
+  current_student.tutor_confirmed = false
   current_student.save!
   redirect_to authenticated_student_root_url
 end
