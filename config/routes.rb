1b261d23bbab72993cb52020c5ef12df82f1b732
bimsara <bp00390@surrey.ac.uk>
2020-05-20 13:59:58 +0100

    Added page and route for schedule

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     root 'tutors/dashboards#home', as: :authenticated_tutor_root
     get 'profile', to: 'tutors/dashboards#profile', as: :authenticated_tutor_profile
     get 'quiz', to: 'tutors/quizzes#show'
-    
+
     get 'setLocation', to: 'tutors/dashboards#setLocation'
     post 'setLocation', to: 'tutors/dashboards#setLocationSubmit'
     get 'setInfo', to: 'tutors/dashboards#setInfo'
@@ -15,17 +15,19 @@ Rails.application.routes.draw do
     post 'acceptStudent', to: 'tutors/dashboards#acceptStudent'
     post 'rejectStudent', to: 'tutors/dashboards#rejectStudent'
     post 'messages', to: 'tutors/dashboards#messages'
+
+    get 'schedule', to: 'tutors/dashboards#schedule'
   end
 
   authenticated :student do
     root 'students/dashboards#home', as: :authenticated_student_root
     get 'profile', to: 'students/dashboards#profile', as: :authenticated_student_profile
-    
+
     post 'pickLanguage', to: 'students/dashboards#pickLanguage'
     post 'requestTutor', to: 'students/dashboards#requestTutor'
     get 'findTutor', to: 'students/dashboards#maps'
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #Home page is the root
