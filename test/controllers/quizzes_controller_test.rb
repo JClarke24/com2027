9d7a77f29b86d70be986373f0b7df4d95a3b3855
Poli <py00103@surrey.ac.uk>
2020-05-26 13:33:50 +0100

    Quiz, Submissions,Reports

--- test/controllers/quizzes_controller_test.rb
+++ test/controllers/quizzes_controller_test.rb
@@ -1,23 +1,31 @@
 require 'test_helper'
 
 class QuizzesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+
+    @tutor = tutors(:tutor1)
+    sign_in @tutor
+  end
+
+
   test "should get index" do
-    get quizzes_index_url
+    get quizzes_url
     assert_response :success
   end
 
   test "should get new" do
-    get quizzes_new_url
+    get quizzes_url
     assert_response :success
   end
 
   test "should get create" do
-    get quizzes_create_url
+    get quizzes_url
     assert_response :success
   end
 
   test "should get destroy" do
-    get quizzes_destroy_url
+    get quizzes_url
     assert_response :success
   end
 
--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -1,28 +1,38 @@
 require 'test_helper'
 
 class SubmissionsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+
+    @student = students(:student1)
+    sign_in @student
+  end
+
+
   test "should get index" do
-    get submissions_index_url
+
+    get submissions_url
     assert_response :success
   end
 
   test "should get new" do
-    get submissions_new_url
+
+    get submissions_url
     assert_response :success
   end
 
   test "should get create" do
-    get submissions_create_url
+    get submissions_url
     assert_response :success
   end
 
   test "should get destroy" do
-    get submissions_destroy_url
+    get submissions_url
     assert_response :success
   end
 
   test "should get show" do
-    get submissions_show_url
+    get submissions_url
     assert_response :success
   end
 
