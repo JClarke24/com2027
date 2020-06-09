cbc23fad473c972d3a1513a89a9035b0659066ea
Jonathan Clarke <jc01663@surrey.ac.uk>
2020-06-09 23:26:21 +0100

    added admin tests

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = admins(:admin1)
+  end
+
+  # Tests empty student shouldn't be saved
+  test 'should not saved invalid admin' do
+    admin = Admin.new
+    admin.save
+    refute admin.valid?
+  end
+
+  # Tests student should be saved
+  test 'should save valid admin' do
+    admin = @admin
+    admin.save
+    assert admin.valid?
+  end
 end
