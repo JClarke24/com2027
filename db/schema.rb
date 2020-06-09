dd08a9924b7a857f59adea64a563f5d0c622a92a
mm01789 <mm01789@surrey.ac.uk>
2020-06-09 17:19:39 +0100

    .

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_05_125933) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_125933) do
     t.string "topic"
     t.string "level"
     t.string "attachment"
-    t.integer "tutor_id"
+    t.bigint "tutor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tutor_id"], name: "index_quizzes_on_tutor_id"
@@ -83,8 +86,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_125933) do
   end
 
   create_table "submissions", force: :cascade do |t|
-    t.integer "student_id"
-    t.integer "tutor_id"
+    t.bigint "student_id"
+    t.bigint "tutor_id"
     t.string "description"
     t.string "submission"
     t.string "feedback"
@@ -124,4 +127,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_125933) do
     t.index ["reset_password_token"], name: "index_tutors_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "quizzes", "tutors"
+  add_foreign_key "submissions", "students"
+  add_foreign_key "submissions", "tutors"
 end
